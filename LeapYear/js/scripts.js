58ceab9882e2ab1ce4f65c0b40659748ee236ba5
Austin Roberts and David Tumpowsky <austin@>
2018-03-28 10:16:42 -0700

    display functioning html that determines if a year input is a leap year

---
+++ LeapYear/js/scripts.js
@@ -0,0 +1,25 @@
+//Backend
+
+var leapYear = function(year) {
+  if ((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)) {
+    return true;
+  // } else if (year % 400 === 0) {
+  //   return true;
+  } else {
+    return false;
+  }
+};
+
+
+
+
+//Front end
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
