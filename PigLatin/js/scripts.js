b8f99d9c0382a1446a4adfc52f94ebae483d1709
Austin Roberts and David Tumpowsky <david@>
2018-03-28 14:24:38 -0700

    locate all the vowels in statement and locate all of the spaces includeing one character after each space

---
+++ PigLatin/js/scripts.js
@@ -0,0 +1,64 @@
+//Backend
+
+var translate = function(sentence) {
+  var vowels = ["a", "e", "i", "o", "u"];
+  var text1 = $("#word").text();
+  console.log(text1);
+  var arr = text1.split("");
+
+  for(i = 0; i < arr.length; i++)
+    for(j = 0; j < vowels.length; j++)
+    console.log(vowels.length)
+};
+
+
+
+//Frontend
+
+$(document).ready(function() {
+  $("form#pig-latin").submit(function(event) {
+    event.preventDefault();
+
+    var sentence = $("input#word").val();
+    var userInput = translate(sentence);
+    $("#result").text(userInput);
+
+
+    // var text1 = $(sentence).text();
+    var vowelArr = ["a", "e", "i", "o", "u"];
+    var arr = sentence.split("");
+    var spaces = [" "];
+    var spacePos =[];
+    // var firstPos =[];
+    var firstLetters = spacePos.map(function(space) {
+      return space + 1;
+    });
+
+    console.log(arr)
+    for (i = 0; i < arr.length; i++) {
+      for (j = 0; j < vowelArr.length; j++) {
+         if (arr[i] == vowelArr[j]) {
+           arr[i] = "-";
+           console.log(arr[i])
+        }
+
+      }
+    }
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i] == spaces) {
+        spacePos.push([i]);
+        console.log(spacePos);
+        console.log(firstLetters);
+      }
+    }
+    // for (i=0; i <)
+
+
+
+
+
+
+
+
+  });
+});
